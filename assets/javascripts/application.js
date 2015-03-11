3292496ce15fb52154ce931e78ede747dfc0f548
Faun <github@faun.me>
2015-03-11 01:25:21 -0700

    Update 2015-03-11T08:25:16.723Z

---
+++ assets/javascripts/application.js
@@ -0,0 +1,11 @@
+
+/*
+Include your files here
+ */
+
+(function() {
+
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcGxpY2F0aW9uLmNvZmZlZSJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUFBOztHQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUEiLCJmaWxlIjoiYXBwbGljYXRpb24uanMiLCJzb3VyY2VSb290IjoiL3NvdXJjZS8iLCJzb3VyY2VzQ29udGVudCI6WyIjIyNcbkluY2x1ZGUgeW91ciBmaWxlcyBoZXJlXG4jIyNcblxuIz0gcmVxdWlyZSBmaWxlbmFtZS5jb2ZmZWVcbiJdfQ==
